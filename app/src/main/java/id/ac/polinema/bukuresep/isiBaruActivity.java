8d520f8f1256a295de0154fb84b00c2a80d51cc7
episkuswono <episkuswono@gmail.com>
2019-12-13 10:20:01 +0700

    buat untuk isi baru resep

---
+++ app/src/main/java/id/ac/polinema/bukuresep/isiBaruActivity.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.bukuresep;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class isiBaruActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_isi_baru);
+    }
+}
