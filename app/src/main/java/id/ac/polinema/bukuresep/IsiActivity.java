17525489844e21eb94c3486f4d960b266cb6ad1e
episkuswono <episkuswono@gmail.com>
2019-12-13 10:38:24 +0700

    insert data resep ke IsiActivity

--- app/src/main/java/id/ac/polinema/bukuresep/IsiActivity.java
+++ app/src/main/java/id/ac/polinema/bukuresep/IsiActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class IsiActivity extends AppCompatActivity {
 
+    public static Object ma;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
